cff6d36d54ac044cce5c11b7fdfbef111d8d9265
AbhishekSoni663 <Abhisheksoni663@gmail.com>
2023-07-12 21:41:52 +0530

    second commit

--- src/FoodDemoComponent/FoodDemo.jsx
+++ src/FoodDemoComponent/FoodDemo.jsx
@@ -4,8 +4,8 @@ import AOS from "aos";
 import "aos/dist/aos.css";
 import { Button, Grid, Typography } from "@mui/material";
 import { Fade, Zoom } from "react-awesome-reveal";
-import whatImg from "../assets/Hero2/whatsapp.png"
-import callImg from "../assets/Hero2/phone-call.png"
+import whatImg from "../assets/Hero2/whatsapp.png";
+import callImg from "../assets/Hero2/phone-call.png";
 
 // import { motion, useTime, useTransform } from "framer-motion";
 const FoodDemo = () => {
@@ -21,63 +21,64 @@ const FoodDemo = () => {
       </span>
       <section className="demo-detail">
         <div className="demo-img">
-          <span className="demo-img1">
-            <span className="img" data-aos="flip-right"></span>
-            <span className="img2" data-aos="flip-left"></span>
-          </span>
+          <div className="demo-img1">
+            <div className="img" data-aos="flip-right"></div>
+            <div className="img2" data-aos="flip-left"></div>
+          </div>
           <span className="demo-img2">
             <span className="img3" data-aos="flip-right"></span>
             <span className="img4" v data-aos="flip-left"></span>
           </span>
         </div>
         <div className="demo-head">
-        
-            <div className="demo-head-detail">
-              <span className="demo-h1">
-                <h1>
-                  Fresh <span style={{ color: "white" }}>& </span>Tasty Food
-                </h1>
-                <p className="de-h1-p">A Sophisticated & Traditional<br/> Cuisine</p>
-              </span>
-              <span className="demo-h2">
-                <p className="de-p">
-                  Our Restaurants is One Of The Most Popular Restaurant with
-                  Unique & Special menu by our passionate chefs with love and
-                  quality with can impress you
-                </p>
+          <div className="demo-head-detail">
+            <span className="demo-h1">
+              <h1>
+                Fresh <span style={{ color: "white" }}>& </span>Tasty Food
+              </h1>
+              <p className="de-h1-p">
+                A Sophisticated & Traditional
+                <br /> Cuisine
+              </p>
+            </span>
+            <span className="demo-h2">
+              <p className="de-p">
+                Our Restaurants is One Of The Most Popular Restaurant with
+                Unique & Special menu by our passionate chefs with love and
+                quality with can impress you
+              </p>
 
-                <br />
-                <span>
-                  <p className="p1">
-                    If Any Querry{" "}
-                    <a href="tel:">
-                      <img
-                        src={callImg}
-                        height={"25px"}
-                        style={{ marginLeft: "15px" }}
-                      />
-                    </a>
-                  </p>
-                  <p className="p2">
-                    {" "}
-                    You Can Get Information on Whatsapp{" "}
-                    <a href="whatsapp://send?phone=+91 7869407389@s.whatsapp.com&message='I Would Like to Chat With You'">
-                      <img
-                        src={whatImg}
-                        height={"25px"}
-                        style={{ marginLeft: "15px" }}
-                      />
-                    </a>
-                  </p>
-                </span>
+              <br />
+              <span>
+                <p className="p1">
+                  If Any Querry{" "}
+                  <a href="tel:">
+                    <img
+                      src={callImg}
+                      height={"25px"}
+                      style={{ marginLeft: "15px" }}
+                    />
+                  </a>
+                </p>
+                <p className="p2">
+                  {" "}
+                  You Can Get Information on Whatsapp{" "}
+                  <a href="whatsapp://send?phone=+91 7869407389@s.whatsapp.com&message='I Would Like to Chat With You'">
+                    <img
+                      src={whatImg}
+                      height={"25px"}
+                      style={{ marginLeft: "15px" }}
+                    />
+                  </a>
+                </p>
               </span>
-              <span className="demo-link">
-                <a link href="#">
-                  View Full Menu
-                </a>
-              </span>
-            </div>
-          
+            </span>
+            <span className="demo-link">
+              <a link href="#">
+                View Full Menu
+              </a>
+            </span>
+          </div>
         </div>
       </section>
       <Grid
